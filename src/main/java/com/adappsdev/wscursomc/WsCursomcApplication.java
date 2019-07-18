7ac7cff096044e8f664e9ff0b6d00b0955e2691b
anderdam <anderdam@gmail.com>
2019-07-18 14:41:07 -0300

    Criando operacao de instanciacao

--- src/main/java/com/adappsdev/wscursomc/WsCursomcApplication.java
+++ src/main/java/com/adappsdev/wscursomc/WsCursomcApplication.java
@@ -1,13 +1,32 @@
 package com.adappsdev.wscursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adappsdev.wscursomc.domain.Categoria;
+import com.adappsdev.wscursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class WsCursomcApplication {
+public class WsCursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsCursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
