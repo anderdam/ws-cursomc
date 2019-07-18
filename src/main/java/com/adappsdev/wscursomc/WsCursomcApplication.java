bde80c02735376437b27c48659b88dceec2ea2d1
anderdam <anderdam@gmail.com>
2019-07-17 22:29:39 -0300

    Projeto criado

---
+++ src/main/java/com/adappsdev/wscursomc/WsCursomcApplication.java
@@ -0,0 +1,13 @@
+package com.adappsdev.wscursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsCursomcApplication.class, args);
+	}
+
+}
