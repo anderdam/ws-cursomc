1baabd52d232e1d6d5aeea90666f1fa22dd52895
anderdam <anderdam@github.com>
2019-08-03 01:43:16 +0000

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/adappsdev/wscursomc/domain/Pedido.java
+++ src/main/java/com/adappsdev/wscursomc/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable{
@@ -30,11 +29,9 @@ public class Pedido implements Serializable{
 	private Date instante;
 	
 	//Associações
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento; //Pedido possui 1 pagamento
-	
-	@JsonManagedReference
+		
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente; // Pedido possui 1 cliente
