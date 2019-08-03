263496053522c88d4406419dcb7b00a4d4cfad22
anderdam <anderdam@github.com>
2019-08-03 01:28:16 +0000

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/adappsdev/wscursomc/domain/Pedido.java
+++ src/main/java/com/adappsdev/wscursomc/domain/Pedido.java
@@ -15,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -24,6 +25,8 @@ public class Pedido implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 	
 	//Associações
@@ -31,6 +34,7 @@ public class Pedido implements Serializable{
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento; //Pedido possui 1 pagamento
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente; // Pedido possui 1 cliente
