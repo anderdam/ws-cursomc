6d7c896a70b2e147e1b4c0b77c421b0f148a3062
anderdam <anderdam@github.com>
2019-08-04 23:03:07 +0000

    Deletando uma categoria

--- src/main/java/com/adappsdev/wscursomc/services/CategoriaService.java
+++ src/main/java/com/adappsdev/wscursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.adappsdev.wscursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.adappsdev.wscursomc.domain.Categoria;
 import com.adappsdev.wscursomc.repositories.CategoriaRepository;
+import com.adappsdev.wscursomc.services.exceptions.DataIntegrityException;
 import com.adappsdev.wscursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);	
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
 	}
 }
